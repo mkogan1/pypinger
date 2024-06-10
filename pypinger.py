db9b0323c939f1832b2695d4996e574a425f0870
Mark Kogan <mkogan@ibm.com>
2024-06-10 18:34:54 +0300

    refactor: Improve pypinger.py script for pinging multiple hosts

--- pypinger.py
+++ pypinger.py
@@ -8,20 +8,33 @@
 
 import sys
 from ping3 import ping, verbose_ping
+import os
+
+if os.path.exists('/tmp/pypinger.log'):
+    os.remove('/tmp/pypinger.log')
+    
 
 def ping_hosts(hosts):
     for host in hosts:
         try:
-            delay = ping(host, unit='ms')
+            delay = ping(host, unit='ms', size=1, timeout=8)
+            if delay is None or delay is False:  # retry mechanism to handle packet loss
+                delay = ping(host, unit='ms', size=1, timeout=8)
             if delay is None or delay is False:
-                print(f'ERR: {host} is not reachable')
+                # print(f'ERR: {host} is not reachable')
+                print(f'❌: {host}')
                 return
             else:
-                print(f'OK: {host} is reachable with delay {int(delay)} ms')
+                with open('/tmp/pypinger.log', 'a') as f:
+                    f.write(f'OK: {host} is reachable with delay {int(delay)} ms\n')
+                    f.close()
+                # print(f'OK: {host} is reachable with delay {int(delay)} ms')
         except Exception as e:
-            print(f'ERR: {host} caused an exception: {e}')
+            # print(f'ERR: {host} caused an exception: {e}')
+            print(f'❌: {host}')
             return
-    print('OK: All hosts are reachable')
+    # print('OK: All hosts are reachable')
+    print('✅')
 
 if __name__ == "__main__":
     ping_hosts(sys.argv[1:])
